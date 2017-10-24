a7ac3147b012b9834f667889a5beeadf106c6e70
Miroslav Bajtoš <mbajtoss@gmail.com>
2017-10-24 10:21:08 +0200

    Add simple servers using different frameworks

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # async-frameworks
 A benchmark comparing performance of async/await in different HTTP frameworks
+
+ - Express@4.16
+ - Koa@2.3 + koa-router@7.2
+ - Fastify@0.30
+ - Hapi@17.0-rc
---
+++ test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const supertest = require('supertest');
+const db = require('./lib/db');
+
+const apps = {
+  express: require('./lib/express'),
+  koa: require('./lib/koa'),
+  fastify: require('./lib/fastify'),
+  hapi: require('./lib/hapi'),
+};
+
+before(db.connect);
+after(db.close);
+
+Object.keys(apps).forEach(name => {
+  describe(`${name} app`, () => {
+    it('works', async () => {
+      const port = await apps[name]();
+      await verify(port);
+    });
+  });
+});
+
+async function verify(port) {
+  return supertest(`http://127.0.0.1:${port}`)
+    .get('/products/4006381333931')
+    .expect(200)
+    .expect([{
+      _id : '59eeec2b52f8816120d53cc0',
+      ean : 4006381333931,
+      name : 'Pencil',
+    }]);
+}
